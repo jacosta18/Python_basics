b4bdcc136471d872ed6935812acbbd9746b0f8b4
Julian H. Acosta <JAcosta@spartaglobal.com>
2019-07-15 16:45:53 +0100

    Completed exercise 2

---
+++ exercise_2_sparta_clients.py
@@ -0,0 +1,23 @@
+# clients
+
+clients =['BAML', 'GoldmanSachs', 'DirectLine', 'Prudential','M&G', 'BO', 'Home Office', 'Fidelity', 'BlackRock', 'Nikko AM']
+# print(clients)
+
+# print(f"BAML",'is a top tier client', 'however', "Goldman Sachs", 'have increased the demand for DevOps engineers with AWS experience',"DirectLine", 'are looking for testers')
+
+# print(array())
+# Exerice 2
+
+print(clients[0],['Is cool client'])
+print(clients[1],['Is hiring rapidly'])
+print(clients[2],['need DevOps'])
+print(clients[3],['Is not looking'])
+print(clients[4],['Is not happy'])
+print(clients[5],['Is has sacked a developer'])
+print(clients[6],['Is nicest clients'])
+print(clients[7],['Is expanding its business in Ireland'])
+print(clients[8],['Is okay'])
+print(clients[9],['are looking for testers'])
+
+#Exersice 3
+
