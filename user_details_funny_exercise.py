4e4ae0cab4b38b3e282c2fa66a2a492cbb73c050
Julian H. Acosta <JAcosta@spartaglobal.com>
2019-07-15 13:50:17 +0100

    Task Completed

---
+++ user_details_funny_exercise.py
@@ -0,0 +1,26 @@
+import datetime
+# This is where I will do my exercise
+# Filipe told me to sudo code first
+
+# Get user first name
+# Save to variable
+# Get user other thing
+# Save somwhere else
+
+# Do some formating (What formating should be done and where?)
+# Out put some data
+
+first_name = input('What is your first_name?')
+last_name = input('What is your last_name?')
+Full_name = first_name + ' ' + last_name
+
+# Entering Most Important date
+most_important_date = input('Most important date in YYYY-MM-DD')
+#
+reason_for_important_date = input('Reason_for_important_date')
+#
+funny_fact = input('Tell me a funny_fact')
+#
+favorite_pokemon = input("What's your favorite_pokemon")
+
+print(f"Welcome (first_name) (last_name) your most important date")
