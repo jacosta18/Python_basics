89c30f2d25d9eb96d2367db7a56d6a6a24d64850
Julian H. Acosta <JAcosta@spartaglobal.com>
2019-07-15 11:19:26 +0100

    Done with data types

--- 01variables.py
+++ 01variables.py
@@ -13,6 +13,10 @@ print (my_variable)
 my_variable = "I'm new content being reassigned to my_variable"
 print (my_variable)
 
+# for the £5 where is the variable/box?
+
+correct_response = 'CPU Registers'
+print('the correct response' was)
 
 
 
---
+++ 02_data_types.py
@@ -0,0 +1,90 @@
+# Data types
+# Computers are stupid.
+# They don't understand context and we need to be specific with data types.
+
+#We can use type () to check data types
+
+#Strings
+    #Lists of Characters bundeled together in a specific order.
+        # using index
+
+print('hello')
+print(type('hello'))
+
+    # Concatenation of strings - Joining of 2 strings
+
+string_a = 'Hello there'
+name_person ='Juan Pier'
+
+print(string_a + '' + name_person)
+
+#Useful methods
+# Lenght
+print(len(string_a))
+print (len(name_person))
+# Strip - removes white spaces
+
+string_num =' 90383 '
+print(type(string_num))
+print(string_num)
+print(string_num.strip())
+
+# .split- it is a method for strips
+#   It splits in a specific location nand output a list (data type)
+#string_text = 'Hello, my name is Felipe, I like turtels'
+#split_string = string_text.split(', ')
+#print(split_string)
+
+# Count / Lower/ upper / capitalize
+
+# text_example = " here is some text, with lots's of text"
+# #Count
+# print(text_example.count('e'))
+# print(text_example.count('text'))
+#
+# #Lower
+# print(text_example.lower())
+#
+# # Upper
+# print(text_example.upper())
+#
+# # Capitalize
+# print(text_example.capitalize())
+# print('PizzaHut'.strip().capitalize())
+#
+# #  Only capitalizes the first character
+
+#string_text = 'Hello, my name is Felipe, I like turtels'
+#split_string = string_text.split('i ')
+#print(split_string)
+
+# # -Get user input and print first and last name
+# # -Get user input / first name
+# first_name =input('What is your first name')
+# # -Save use input to variable
+# # -Get user last name  and save it to variable
+# last_name = input('what is your last name')
+# # -Joing the two
+# # - Let us use concatenation
+# full_name = first_name + ''+ last_name
+# print(full_name)
+# # - let us use interpolation
+# welcome_message = f"Hi {full_name}, you are very welcome!"
+# print(welcome_message)
+# # -print
+#
+# user_input_first_name = input('what is your first name')
+# print(user_input_first_name)
+
+# Count/ control / turns into comments
+
+
+
+
+
+
+
+
+
+# Casting
+
