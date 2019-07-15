73e56e931d7403f3d063a2ac95dea2abadc852c5
Julian H. Acosta <JAcosta@spartaglobal.com>
2019-07-15 14:19:57 +0100

    Data types Booleans

---
+++ data_types_booleans.py
@@ -0,0 +1,44 @@
+# Booleans
+#
+
+
+var_true = True
+var_false = False
+
+print(type(var_true))
+print(type(var_true))
+
+# When we equate / evaluate something we get a boolean as a response
+#Logical operator
+# == / != / <> / >= / <=
+weather = 'Rainy'
+
+print(weather == 'sunny')
+print(weather == 'Rainy')
+
+#Logical **AND** & **OR**
+# Evaluates two sides and both have to be true for it to return true
+
+print(weather == 'Rainy' and weather == 'Sunny')
+print(True and True)
+# print(True and False)
+print(weather == 'Rainy' and weather == 'Rainy')
+print(True and False)
+
+#Logical OR - One of the side equasion hace to be True to retrun True
+print('>Testing logical or:')
+print(True or False)
+print(False or True)
+
+# Some methods or function can return booleans
+potential_number = '10'
+print(potential_number.isnumeric())
+print(int(potential_number).isinteger())
+
+text = 'Hello World!'
+print(text.startswith('H'))
+print (text.startswith('h'))
+print('Testing.endswith(arg)')
+print(text[-1] == '?') # strings are list of characters. -1 represents the last index in said list.
+print(text.endswith('!'))
+print(text.endswith('?'))
