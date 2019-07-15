9ff7c26cd6444f9d8a32471f7f76d31d1195a728
Julian H. Acosta <JAcosta@spartaglobal.com>
2019-07-15 18:09:10 +0100

    Completed task 2

--- 01variables.py
+++ 01variables.py
@@ -15,8 +15,24 @@ print (my_variable)
 
 # for the £5 where is the variable/box?
 
-correct_response = 'CPU Registers'
-print('the correct response' was)
+# clients
 
+clients =['BAML', 'GoldmanSachs', 'DirectLine', 'Prudential','M&G', 'BO', 'Home Office', 'Fidelity', 'BlackRock', 'Nikko AM']
+# print(clients)
 
+# print(f"BAML",'is a top tier client', 'however', "Goldman Sachs", 'have increased the demand for DevOps engineers with AWS experience',"DirectLine", 'are looking for testers')
+
+# print(array())
+# Exerice 2
+
+print(f"BAML",'is a cool client.')
+print(f"GoldmanSachs", 'is in desperate need for data analysts.')
+print(f"DirectLine",'need DevOps.')
+print(f"Prudential",'Is not happy.')
+print(f"M&G",'Is has sacked a developer.')
+print(f"BO",'Is nicest clients.')
+print(f"Home Office",'Is expanding its business in Ireland.')
+print(f"Fidelity",'Is okay.')
+print(f"BlackRock",'are looking for testers.')
+print(f"Nikko AM",'Is not looking to hire.')
 
