3e91481c28179b05ba7d1291f5c6212a6b6adeb5
Julian H. Acosta <JAcosta@spartaglobal.com>
2019-07-15 11:49:05 +0100

    Break from Intergers

---
+++ 03_data_types_interger.py
@@ -0,0 +1,68 @@
+# # Numericals - Intergers, big Intergers and complex numbers
+#
+# # Intergers Whole numbers
+# my_int = 13
+# print(type(my_int))
+# print(type(13)) # Not the same as print(type('13')) --> STRING!
+#
+# # Floats are composite numbers
+#
+# print(13.1)
+# print(type(13.1))
+
+# Long and complex
+# less used check syntax when needed
+
+# Operators
+# Add
+num_a=10
+num_b=7.3
+print(num_a+num_b)
+
+#Subtraction
+print(num_a - num_b)
+
+#Multiply
+print(num_a*num_b)
+
+#Divide
+print(num_a/2)
+
+# Modulus ##>> Check for remainders
+print(10 % 2)
+# Checking if even
+print((12 %2) == 0)
+print(18 % 3)
+
+# Logical Operators
+# They evaluate two side and return boolean
+# Check IF equal ==
+a = 10
+b = 13
+print(a == b)
+print (a == 10)
+
+# CHecking if bigger and smaller than
+print(a < b)
+print(a>9)
+print('.....')
+
+print(b < a)
+print(b < 15)
+
+# Checking bigger/smaller or equal
+
+print(a > 10)
+print(a>=10)
+
+# Check if not something
+print(a != 9) # a is not 9, it is 10. so it retruns true
+
+# Increment
+counter = 0
+counter += 1
+
+print(counter)
+counter += 1
+print(counter)
+
