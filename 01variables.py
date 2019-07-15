d0eea502db1540d3df51fd19114ad8264eebad19
Julian H. Acosta <JAcosta@spartaglobal.com>
2019-07-15 10:01:10 +0100

    Data file ammended

---
+++ 01variables.py
@@ -0,0 +1,18 @@
+# Variables in Python
+
+# It's like a box
+# Has a name
+# Stores things inside.
+my_variable = 'Hello there'
+
+# Variable with a string being printed to console
+print (my_variable)
+
+# Reasaign a variable
+
+my_variable = "I'm new content being reassigned to my_variable"
+print (my_variable)
+
+
+
+
