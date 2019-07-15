b86a33ab69620651e982cb4b7696a1b417a069f3
Julian H. Acosta <JAcosta@spartaglobal.com>
2019-07-15 16:17:05 +0100

    Dict completed

--- 07_data_types_dict.py
+++ 07_data_types_dict.py
@@ -1,4 +1,62 @@
 # Dictionaries AKA Hashes
+# Work with key: value pairs. As apposed to using indexes.
+
+#Delcating a hash/dictionary
+pika = {}
+
+# Dictionaries work with key:values.
+
+pika = {
+    'name': 'Derik Dice',
+    'pokemon':'pikachu',
+    'age':17,
+    'personality': 'Grumpy'
+}
+
+print(pika)
+
+#Access information using the keys
+print(pika['age'])
+print(pika['pokemon'])
+
+#re-assaign values using keys
+pika['age'] = 18
+print(pika['age'])
+
+# Adding a key value pair
+
+pika['colour']='Yellowish'
+print(pika)
+
+#Creating key value for first and last name
+full_name=pika['name'].split()
+print(full_name)
+first_name = full_name[0]
+print(first_name)
+pika['first_name'] = first_name
+pika ['last_name'] = full_name[1]
+print(pika)
+
+# Embedded data
+
+pika = {
+    'name': 'Derik Dice',
+    'pokemon':'pikachu',
+    'age':17,
+    'personality':{
+        'frumpy': 10,
+        'lovely': 2}
+}
+
+print(pika['personality']['grumpy'])
+print(pika['personality'][0])
+
+# Important methods
+keys = pika.keys()
+print(keys)
+
+values = pika.values()
+print(values)
 
 
 
