4f04aebe54d13036887066dac085d35192685700
Julian H. Acosta <JAcosta@spartaglobal.com>
2019-07-16 10:58:04 +0100

    if statements

---
+++ 09_control_flow.py
@@ -0,0 +1,30 @@
+# Control flow - IF statements
+# Syntax - indentation matters
+#if <condition>:
+    # Block of code
+# else:
+    # Block of code
+# else just print the false outcome
+
+age = 70
+
+if age >= 17:
+    print('Your age is above 18, you can vote! :D')
+elif age >= 18:
+    print("You can do everything, just take it easy")
+elif age >= 16:
+    print('You can buy the lottery')
+else:
+    print('You are under age to vote')
+
+########################################################
+
+weather = input("what's the weather like?").lower().strip()
+
+if weather == 'rainy' or weather == 'rainy':
+    print('take an unmbrela and a jacket')
+elif weather == 'sunny':
+    print('take glasses wear sunscreen')
+else:
+    print('Live your best life')
+
