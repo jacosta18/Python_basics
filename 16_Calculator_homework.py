80fa7b58852e64194e473c8692e78a51292c92be
Julian H. Acosta <JAcosta@spartaglobal.com>
2019-07-16 21:32:05 +0100

    Homework

--- 16_Calculator_homework.py
+++ 16_Calculator_homework.py
@@ -1,51 +1,71 @@
 # Creating a calculator
 
-# Lets start with the multiplication function
-
-# def result(num_1,num_2):
-#     if 'x' in num_1 * 'y' in num_2:
-#         return 'z'
+# def add(x, y):
+#     return x + y
+#
+# def subtract(x, y):
+#     return  x - y
+#
+# def multiply(x, y):
+#     return x * y
+#
+# def divide(x, y):
+#     return x / y
+#
+# print("select your option")
+# print("1. Addition")
+# print("2. Subtract")
+# print("3. Mulitply")
+# print("4. Divide")
+#
+# choice = input("Enter choice 1/2/3/4):")
+#
+# num1 = float(input("Enter the first number: "))
+# num2 = float(input("Enter the second number: "))
+#
+# while True:
+#     if choice == '1':
+#         print(float(num1),"+", float(num2),"=", add(float(num1),float(num2)))
+#     elif choice == '2':
+#             print(float(num1), "-", float(num2), "=", subtract(float(num1),float(num2)))
+#     elif choice == '3':
+#             print (float(num1), "*", float(num2),"=", multiply(float(num1),float(num2)))
+#     elif choice == '4':
+#             print(float(num1), "/", float(num2), "=", divide(float(num1),float(num2)))
 #     else:
-#         print("wrong result")
+#         print("Invalid input")
+#     word = input("New operation? YES / NO:".lower())
+#     if word == 'YES':
+#         print("Let's GO!, PRESS CTRL,SHIFT, F10")
+#     else:
+#         print("See you later!")
+#     break
+#-----------------------------------------------------------------------------------------------
 
-def add(x, y):
-    return x + y
+# This is an add function:
 
-def subtract(x, y):
-    return  x - y
+def multiply (x,y):
+    return x*y
 
-def multiply(x, y):
-    return x * y
+print(multiply(100,40)==4000)
 
-def divide(x, y):
-    return x / y
+# This is subtraction
 
-print("select your option")
-print("1. Addition")
-print("2. Subtract")
-print("3. Mulitply")
-print("4. Divide")
+def addition (x,y):
+    return x+y
+print(addition(20,5)==25)
 
-choice = input("Enter choice 1/2/3/4):")
+# This is division
 
-num1 = float(input("Enter the first number: "))
-num2 = float(input("Enter the second number: "))
+def subtraction(x,y):
+    return x-y
+print(subtraction(23,3)==20)
 
-while True:
-    if choice == '1':
-        print(float(num1),"+", float(num2),"=", add(float(num1),float(num2)))
-    elif choice == '2':
-            print(float(num1), "-", float(num2), "=", subtract(float(num1),float(num2)))
-    elif choice == '3':
-            print (float(num1), "*", float(num2),"=", multiply(float(num1),float(num2)))
-    elif choice == '4':
-            print(float(num1), "/", float(num2), "=", divide(float(num1),float(num2)))
-    else:
-        print("Invalid input")
-    word = input("New operation? YES / NO:".lower())
-    if word == 'YES':
-        print("Let's GO!, PRESS CTRL,SHIFT, F10")
-    else:
-        print("See you later!")
-    break
+# This is division
+
+def division(x,y):
+    return x/y
+print(division(50,5)==10)
+
+#------------------------------------------------------------------------------
 
---
+++ 17_Area_Triangle.py
@@ -0,0 +1,35 @@
+import math
+# Area of a square
+# def multiply(x, y):
+#     return x * y
+
+# Area of triangle
+# def divide(x, y):
+#     return x * y/2
+#
+#
+# a = float(input("Enter the length of triangle: "))
+# b = float(input("Enter the height of triangle: "))
+#
+# area = ((float(a) * float(b)) / 2)
+#
+# print(area)
+
+#--------------------------------------------------------------------------#
+
+##### Phase 1
+
+def triangle_area(x,y):
+    return ((x*y)/2)
+print(triangle_area(20,2)==20)
+
+##### Phase 2
+
+def circle_area(radius):
+    return math.pi * float(radius) ** 2
+print(circle_area(23)==78.54)
+
+#### Phase 3
+
+
+
