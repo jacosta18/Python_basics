e06455b061bdb98f6fcc0156d4e9c265e939acc2
Julian H. Acosta <JAcosta@spartaglobal.com>
2019-07-16 11:14:46 +0100

    First commit

--- 09_control_flow.py
+++ 09_control_flow.py
@@ -19,12 +19,23 @@ else:
 
 ########################################################
 
+# weather = input("what's the weather like?").lower().strip()
+#
+# if weather == 'rainy' or weather == 'rainy':
+#     print('take an unmbrela and a jacket')
+# elif weather == 'sunny':
+#     print('take glasses wear sunscreen')
+# else:
+#     print('Live your best life')
+
 weather = input("what's the weather like?").lower().strip()
 
-if weather == 'rainy' or weather == 'rainy':
-    print('take an unmbrela and a jacket')
-elif weather == 'sunny':
-    print('take glasses wear sunscreen')
+if weather == 'rainy and stormy':
+    print('Take a jacket when it is rainy and stormy')
+elif weather == 'foggy':
+    print('Take an umbrella, because you never know')
+elif weather == 'rainy':
+    print('Take an umbrella only when it is rainy')
 else:
     print('Live your best life')
 
